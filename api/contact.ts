fb07bbc58c5f6ab6c280b0deb7fef4fb9d9b50d4
jaewoo-rain <202017499@jbnu.ac.kr>
2026-03-05 18:56:52 +0900

    이메일

--- api/contact.ts
+++ api/contact.ts
@@ -79,7 +79,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const result = await resend.emails.send({
       // from: "화경마케팅 <contact@hwakyungmarketing.com>",
-      from: "onboarding@resend.dev",
+      from: "202017499@jbnu.ac.kr",
       to: "202017499@jbnu.ac.kr",
       subject: `[화경마케팅 문의] ${safeName}`,
       replyTo: email,
